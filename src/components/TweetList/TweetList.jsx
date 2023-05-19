138e956d5e06f4ecfcc9e3bd0289868a24a2a374
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-05-19 20:38:01 +0300

    create: load more button

--- src/components/TweetList/TweetList.jsx
+++ src/components/TweetList/TweetList.jsx
@@ -7,21 +7,40 @@ import { useSelector, useDispatch } from 'react-redux';
 import style from './TweetList.module.css';
 
 import TweetCard from '../TweetCard/TweetCard';
+import LoadMoreBtn from '../LoadMoreBtn/LoadMoreBtn';
 
 const TweetList = () => {
   const dispatch = useDispatch();
-
   const users = useSelector(getUsers);
+
+  const usersPerPage = 3;
+  const [visibleItems, setVisibleItems] = useState(usersPerPage);
 
   useEffect(() => {
     dispatch(fetchUsers());
   }, [dispatch]);
 
+  const isUsers = !(users.length === undefined);
+  if (!isUsers) {
+    return null;
+  }
+
+  const visibleUsers = users.slice(0, visibleItems);
+  const isLoadMore = users.length <= visibleUsers.length;
+
+  const handleLoadMore = () => {
+    setVisibleItems(prevVisibleItems => prevVisibleItems + usersPerPage);
+  };
+
   return (
-    <ul className={style.tweetList}>
-      {!(users.length === undefined) &&
-        users.map(user => <TweetCard key={user.id} user={user} />)}
-    </ul>
+    <>
+      <ul className={style.tweetList}>
+        {visibleUsers.map(user => (
+          <TweetCard key={user.id} user={user} />
+        ))}
+      </ul>
+      <LoadMoreBtn onLoadMore={handleLoadMore} isDisabled={isLoadMore} />
+    </>
   );
 };
 
