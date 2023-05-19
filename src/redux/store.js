31650060a320596e1d695c10f4626a83488e9f20
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-05-19 21:33:19 +0300

    create: filter store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import { usersReducer } from './users/users.slice';
-// import { filterReducer } from '../redux/filterSlice';
+import { filterReducer } from './users/filter.slice';
 
 export const store = configureStore({
   reducer: {
     users: usersReducer,
-    // filter: filterReducer,
+    filter: filterReducer,
   },
 });
