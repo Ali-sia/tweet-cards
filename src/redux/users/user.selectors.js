321a4ab8d0a3ba5ceea8e3dd7c7176c400168ffb
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-05-19 21:33:41 +0300

    create: filter selector

--- src/redux/users/user.selectors.js
+++ src/redux/users/user.selectors.js
@@ -1,3 +1,22 @@
+import { createSelector } from '@reduxjs/toolkit';
+import { statusFilter } from './filter.constants';
+
 export const getUsers = state => state.users.users;
 export const getIsLoading = state => state.users.isLoading;
 export const getError = state => state.users.error;
+
+export const getFilter = state => state.filter.status;
+
+export const selectVisibleUsers = createSelector(
+  [getUsers, getFilter],
+  (users, statusFilters) => {
+    switch (statusFilters) {
+      case statusFilter.unsubscribed:
+        return users.filter(user => !user.followed);
+      case statusFilter.subscribed:
+        return users.filter(user => user.followed);
+      default:
+        return users;
+    }
+  }
+);
