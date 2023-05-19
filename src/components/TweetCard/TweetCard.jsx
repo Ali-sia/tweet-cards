4cca048862ebe586435019149e56490d6c2ab0d0
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-05-19 16:29:01 +0300

    update: card (get data from api)

--- src/components/TweetCard/TweetCard.jsx
+++ src/components/TweetCard/TweetCard.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import style from './TweetCard.module.css';
 
 import logoGoIt from '../../assets/images/logoGoIt.svg';
@@ -8,7 +9,11 @@ const TweetCard = () => {
   return (
     <>
       <h1>tweet card</h1>
+const TweetCard = ({ user }) => {
+  const { id, name, tweets, followers, avatar, followed } = user;
 
+  return (
+    <li>
       <div className={style.tweetCard}>
         <div className={style.logo}>
           <img src={logoGoIt} alt="logo"></img>
@@ -19,18 +24,32 @@ const TweetCard = () => {
         <div className={style.avatarBorder}>
           <div className={style.avatar}>
             <img src={defaultCardAvatar} alt="user avatar"></img>
+            <img
+              src={avatar ? avatar : defaultCardAvatar}
+              alt="user avatar"
+            ></img>
           </div>
         </div>
         <div className={style.cardSeparator}></div>
         <p className={style.tweets}>
           <span> 777 </span> tweets
+          <span> {tweets} </span> tweets
         </p>
         <p className={style.followers}>
           <span> 100,500 </span> Followers
+          <span> {followers} </span>Followers
         </p>
         <button className={style.followBtn}>Follow</button>
+        <button
+          className={`${style.followBtn} ${followed && style.activeFollowBtn}`}
+          onClick={() => {
+          }}
+        >
+          {followed ? 'Following' : 'Follow'}
+        </button>
       </div>
     </>
+    </li>
   );
 };
 
