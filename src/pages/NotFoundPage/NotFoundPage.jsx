6d7fe045154cd6704375896acf4f5988ed69c90c
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-05-21 00:02:52 +0300

    create: 404 page

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,7 +1,15 @@
+import NotFound from '../../assets/images/NotFound.jpg';
+import style from './NotFoundPage.module.css';
+
 const NotFoundPage = () => {
   return (
-    <div>
-      <h1>404</h1>
+    <div className={style.container}>
+      <h3>404</h3>
+      <h3>We can not find this page((</h3>
+
+      <div className={style.imgContainer}>
+        <img src={NotFound} alt="cat not found"></img>
+      </div>
     </div>
   );
 };
