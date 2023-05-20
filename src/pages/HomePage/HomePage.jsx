ab25f97f28ea1282d2f89e6371866ade5ab89668
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-05-20 23:35:05 +0300

    create: pages for routing

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,51 @@
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta iure
+        explicabo fugit quasi culpa incidunt expedita cumque provident sunt
+        quisquam voluptas dolorum doloribus facilis unde quidem, nam
+        reprehenderit deserunt omnis?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta iure
+        explicabo fugit quasi culpa incidunt expedita cumque provident sunt
+        quisquam voluptas dolorum doloribus facilis unde quidem, nam
+        reprehenderit deserunt omnis?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta iure
+        explicabo fugit quasi culpa incidunt expedita cumque provident sunt
+        quisquam voluptas dolorum doloribus facilis unde quidem, nam
+        reprehenderit deserunt omnis?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta iure
+        explicabo fugit quasi culpa incidunt expedita cumque provident sunt
+        quisquam voluptas dolorum doloribus facilis unde quidem, nam
+        reprehenderit deserunt omnis?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta iure
+        explicabo fugit quasi culpa incidunt expedita cumque provident sunt
+        quisquam voluptas dolorum doloribus facilis unde quidem, nam
+        reprehenderit deserunt omnis?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta iure
+        explicabo fugit quasi culpa incidunt expedita cumque provident sunt
+        quisquam voluptas dolorum doloribus facilis unde quidem, nam
+        reprehenderit deserunt omnis?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta iure
+        explicabo fugit quasi culpa incidunt expedita cumque provident sunt
+        quisquam voluptas dolorum doloribus facilis unde quidem, nam
+        reprehenderit deserunt omnis?
+      </p>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ src/pages/TweetPage/TweetPage.jsx
@@ -0,0 +1,11 @@
+import TweetList from '../../components/TweetList/TweetList';
+
+const TweetPage = () => {
+  return (
+    <div>
+      <TweetList />
+    </div>
+  );
+};
+
+export default TweetPage;
